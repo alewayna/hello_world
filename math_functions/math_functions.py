5e85fed537d3fd991471ca17e0358f262067b17f
Alejandro Martinez <alewayna@gmail.com>
2022-06-11 12:37:01 -0300

    math_functions

---
+++ math_functions/math_functions.py
@@ -0,0 +1,19 @@
+# sum function
+def sum_function(a, b):
+    sum_sum = a + b
+    return sum_sum
+
+# substract function
+def sub_function(a, b):
+    subtract = a - b
+    return subtract
+
+# multiply function
+def multi_function(a, b):
+    multiply = a * b
+    return multiply
+
+# divide function
+def div_function(a, b):
+    division = a / b
+    return division
